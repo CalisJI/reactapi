2da9e4ae714fef00d906bdcfe6331db2ea0893e9
= <=>
2022-10-11 22:11:29 +0700

    mqldataapi first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "calis",
+});
+connection.connect(function (err) {
+  err ? console.log(err) : console.log(connection);
+});
+var dateTime = "";
+const timer = setInterval(() => {
+  var today = new Date();
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  dateTime = date + " " + time;
+}, 1000);
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: dateTime });
+});
+app.get("api/test", (req, res) => {
+  var sql = "SELECT * FROM news ORDER BY id DESC";
+  connection.query(sql, function (err, results) {
+    if (err) console.log(err);
+    res.json({ news: results });
+  });
+});
+app.listen(4000, () => console.log("App listening on port 4000"));
